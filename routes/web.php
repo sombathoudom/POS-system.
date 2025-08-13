570840451c9f62043eef5f4757a420a3f34900fa
Oudom <sombathoudom001@gmail.com>
2025-08-14 01:03:20 +0700

    refactor(routes): redirect home route to dashboard
    - Changed the home route to redirect users to the dashboard instead of rendering the welcome page.
    - This update enhances user navigation by directing authenticated users to the main application interface.

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SaleTransactionController;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+    return redirect('dashboard');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     // Route::get('dashboard', function () {
